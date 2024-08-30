4436336b3fd7324fb9b73997c11a52e17b629833
creditvilleNg <chioma.chukelu@creditville.ng>
2024-08-30 18:13:16 +0100

    update

---
+++ src/main/java/interswitch/assessment/apigatewayfintechdigitalonly/ApigatewayfintechdigitalonlyApplication.java
@@ -0,0 +1,15 @@
+package interswitch.assessment.apigatewayfintechdigitalonly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApigatewayfintechdigitalonlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApigatewayfintechdigitalonlyApplication.class, args);
+    }
+
+}
